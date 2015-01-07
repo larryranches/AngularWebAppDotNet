5a2bbfa1fdb72474b831ffa48f9a75ec623ce4b3
LarryWorkStation <rancheslp@outlook.com>
2015-01-07 14:39:01 -0800

    Adding sql file

---
+++ UserTable.sql
@@ -0,0 +1,58 @@
+﻿create table [dbo].[User] (
+	UserId UNIQUEIDENTIFIER,
+	FirstName VARCHAR(50),
+	LastName VARCHAR(50),
+	Email VARCHAR(50),
+	country VARCHAR(50),
+	ModifiedOn DATETIME
+);
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('f6e752f6-bd7e-4863-8d21-0d90b94883c0', 'Jose', 'Harris', 'jharris@flashset.com', 'Trinidad and Tobago', '07/29/2013');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('ad9ccb75-134b-481f-945d-3a031bfe656d', 'Kathy', 'Simpson', 'ksimpson@mymm.name', 'Myanmar', '06/30/2014');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('5a877cfb-bed4-4dc7-b669-3aafda1d97dd', 'Jerry', 'Holmes', 'jholmes@agimba.mil', 'Turkmenistan', '01/05/2014');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('981b1cfa-6d79-4157-8372-286ef97fa176', 'Peter', 'Little', 'plittle@dabfeed.com', 'Sudan', '03/02/2014');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('70f78d6b-6d50-42fc-84a5-4693ed48ac73', 'Sara', 'Mendoza', 'smendoza@skippad.name', 'Turkey', '06/18/2014');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('362c8b0d-ead6-4953-afa9-b55fa4df63b6', 'Frank', 'Simmons', 'fsimmons@nlounge.com', 'Saint Helena', '01/18/2014');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('8ffb8340-2b99-41af-ba61-ee7fd1976c90', 'Richard', 'Mills', 'rmills@avavee.mil', 'Falkland Islands (Malvinas)', '02/05/2014');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('bc1c7b0c-57d3-4ddc-8c10-58b8ce13c21c', 'Ralph', 'Simmons', 'rsimmons@thoughtblab.mil', 'US Minor Outlying Islands', '07/26/2013');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('0e8fa45e-5f75-4121-a1f9-40772b193fd0', 'Judy', 'Bradley', 'jbradley@photojam.com', 'Jordan', '05/30/2014');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('0560f010-f1ac-4211-be2c-00ceec13a0cc', 'Elizabeth', 'Jacobs', 'ejacobs@feedfish.gov', 'Lithuania', '09/28/2013');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('d44f68c1-31c2-4ee0-8c0a-d3b95fc3a349', 'Dorothy', 'Willis', 'dwillis@oodoo.info', 'Korea, South', '01/24/2014');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('c7ba1278-36f4-45a6-8180-470688a9d668', 'Ruth', 'Garcia', 'rgarcia@fiveclub.info', 'Fiji', '05/31/2014');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('a60c31af-bc1a-4c8a-b84a-a95e4e9b482e', 'Andrea', 'Lane', 'alane@trunyx.info', 'Nicaragua', '03/07/2014');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('3192f56d-f8a4-4735-8ca7-1ce0358eac43', 'Joyce', 'Morrison', 'jmorrison@meemm.name', 'Falkland Islands (Malvinas)', '12/22/2013');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('2da9e1f7-93a6-43a1-9002-b214f1a148f5', 'Stephanie', 'Brooks', 'sbrooks@podcat.name', 'Turkey', '07/20/2013');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('d5e93375-ec7f-48c3-8a18-f8bc42f134cd', 'Catherine', 'Davis', 'cdavis@realmix.biz', 'Russia', '05/01/2014');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('2eb022a5-f32f-4bc6-8ace-5793885fd5f5', 'Carlos', 'Matthews', 'cmatthews@skinder.com', 'Saint Helena', '07/27/2013');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('459f0d43-242a-4e27-8a93-f3c825a7b115', 'Keith', 'Fields', 'kfields@kazu.name', 'Saint Kitts and Nevis', '08/30/2013');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('87303a20-b97c-43c2-8f2e-2cb71de7b11a', 'Joan', 'Henry', 'jhenry@divanoodle.com', 'Singapore', '10/10/2013');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('94fbe8ef-3ecf-4401-987e-9b2f5645941b', 'Linda', 'Johnston', 'ljohnston@realblab.biz', 'Ghana', '12/17/2013');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('1c291b6a-10d3-4394-8ae4-612d21941f9a', 'Carl', 'Hall', 'chall@zoomzone.mil', 'Togo', '07/20/2013');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('a2c1beea-1c8e-43e5-8efa-3c0942661898', 'Jerry', 'Ellis', 'jellis@livepath.mil', 'France', '04/13/2014');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('aa5b7830-ba02-475c-9db9-f393c2d40f8c', 'Janice', 'Pierce', 'jpierce@quimm.name', 'El Salvador', '05/28/2014');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('4a01bba8-f79a-4ed0-9cc7-d213065e8759', 'Jack', 'Palmer', 'jpalmer@roodel.info', 'Sri Lanka', '04/11/2014');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('a509ba5d-20dc-4e8f-b3ab-0153536ee28a', 'Norma', 'Nichols', 'nnichols@gabtype.info', 'Jordan', '08/23/2013');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('06e0b83d-b0eb-49a6-9125-485a8ed3f3d5', 'Helen', 'Nguyen', 'hnguyen@centimia.info', 'Spain', '05/20/2014');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('4b96e5ab-1aa2-4938-898a-0b30f528d1d6', 'Christopher', 'Marshall', 'cmarshall@jetpulse.info', 'Rwanda', '10/01/2013');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('1058343d-c1cc-4ca2-8b1e-60f48047d9bd', 'Henry', 'Harris', 'hharris@tavu.gov', 'Zimbabwe', '06/07/2014');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('47f212d8-7271-40f7-938d-128aa7a71ba2', 'Jesse', 'Larson', 'jlarson@thoughtstorm.mil', 'Yugoslavia', '11/07/2013');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('26d1c740-51dc-490c-b61a-521d685b0ed6', 'Kathryn', 'Alvarez', 'kalvarez@abatz.mil', 'Ascension Island', '10/09/2013');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('9c69131a-5e10-4671-b652-f91c3604081b', 'Amy', 'Hernandez', 'ahernandez@yacero.info', 'Mozambique', '01/30/2014');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('a628e9cb-e6b1-4ad2-99ef-dc0d237461f9', 'Sara', 'Gray', 'sgray@jaloo.org', 'France', '11/28/2013');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('36a6db8f-5459-4cc8-b736-5e2ed9998355', 'Roger', 'Mitchell', 'rmitchell@leexo.gov', 'Norfolk Island', '10/20/2013');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('1e3aeeab-6b6c-47a3-8647-ebc7f49a8906', 'Katherine', 'Cruz', 'kcruz@twitternation.org', 'Belgium', '01/08/2014');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('8c5a5759-1dfc-4e28-a601-5c6ba20c0fe1', 'Joyce', 'Hunt', 'jhunt@shuffletag.info', 'Morocco', '12/28/2013');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('d8261110-674d-45f0-add2-b87cceebf02f', 'Aaron', 'Perez', 'aperez@jabberbean.com', 'Anguilla', '07/12/2014');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('33c7628c-1147-4db9-a88d-07e343dd50af', 'Willie', 'Gutierrez', 'wgutierrez@photolist.mil', 'Qatar', '03/05/2014');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('17fbb5fb-5e96-4d2e-9be9-aaecd2c23afa', 'Arthur', 'Green', 'agreen@quire.org', 'Seychelles', '03/25/2014');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('ba757ee1-1e1c-488b-b442-876dd98c55e0', 'Evelyn', 'Gibson', 'egibson@shuffledrive.name', 'Vietnam', '04/30/2014');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('6ed3b3ce-e3b8-4092-9488-3996ffb56b02', 'Cynthia', 'Roberts', 'croberts@wikido.edu', 'Paraguay', '10/23/2013');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('9e87c0d9-5bee-4913-8b96-6fbe91de89f7', 'Helen', 'Arnold', 'harnold@rhynoodle.com', 'Liberia', '10/23/2013');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('a7b62b7a-c890-4330-916d-a5d69e377a75', 'Bonnie', 'Arnold', 'barnold@oloo.info', 'Romania', '04/15/2014');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('73f058fc-9e3d-4669-87a4-bceabfd7f206', 'Janet', 'Sanchez', 'jsanchez@kwinu.net', 'Mauritius', '05/17/2014');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('5110fe15-aeb8-471f-8e64-9f00ee380a5e', 'Bobby', 'Larson', 'blarson@mynte.gov', 'Fiji', '05/13/2014');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('a50ef25e-b800-4f44-8e80-694885003a38', 'Judith', 'Weaver', 'jweaver@innojam.biz', 'France', '02/15/2014');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('e3a83774-1784-4174-958e-3209acf8c0e5', 'Jesse', 'Woods', 'jwoods@edgeify.org', 'Barbados', '05/29/2014');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('f64b735c-102d-400d-a93d-575f299450bf', 'Matthew', 'Gibson', 'mgibson@realmix.edu', 'Cote d''Ivoire', '02/03/2014');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('81ccab8c-f515-45e4-99a1-d340f1575cf0', 'Karen', 'Weaver', 'kweaver@yodo.biz', 'Kiribati', '06/09/2014');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('df85853b-96ed-43fc-91b0-9329e3b58c99', 'Harry', 'Hamilton', 'hhamilton@flipopia.com', 'Christmas Island', '12/24/2013');
+insert into [dbo].[User] (UserId, FirstName, LastName, Email, country, ModifiedOn) values ('16f35a5a-ce75-459a-a436-750068923426', 'Catherine', 'Brooks', 'cbrooks@jatri.org', 'Benin', '08/03/2013');
